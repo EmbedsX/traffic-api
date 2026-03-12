1278de03a126f5f024d244a835679f0caa8e4515
EmbedsX <shubham282018@gmail.com>
2026-03-12 21:56:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -98,8 +98,9 @@ res.json(lastResult);
 
 // ------------------------
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 10000;
 
 app.listen(PORT,()=>{
 console.log("Server running on port",PORT);
 });
+
